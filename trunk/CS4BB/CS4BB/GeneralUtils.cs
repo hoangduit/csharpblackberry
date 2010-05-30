9f069b7511339dd933a7923f02fdc15c65aaf061
lenniedg <lenniedg@e78c83d8-92f2-14f2-9b48-f43d6f5d67e1>
2010-05-30 10:48:17 +0000

    Some missing files

---
+++ trunk/CS4BB/CS4BB/GeneralUtils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CS4BB
+{
+    public sealed class GeneralUtils
+    {
+        /// <summary>
+        /// Get the command line argument at the given index
+        /// </summary>
+        /// <param name="aArguments"></param>
+        /// <param name="aParameterIndex"></param>
+        /// <returns></returns>
+        public static string GetParameter(string[] aArguments, int aParameterIndex)
+        {
+            try
+            {
+                return aArguments[aParameterIndex].Trim();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new ArgumentException("Pass the directory location to convert all C# files.");
+            }
+        }
+
+        /// <summary>
+        /// Get the assembly version number
+        /// </summary>
+        /// <returns></returns>
+        public static string GetVersionNumber()
+        {
+            Assembly myAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName myAssemblyName = myAssembly.GetName();
+            return myAssemblyName.Version.ToString();
+        }
+
+        /// <summary>
+        /// Write error to error file
+        /// </summary>
+        /// <param name="aSourceDirectory"></param>
+        /// <param name="aErrors"></param>
+        public static void WriteErrorFile(String aSourceDirectory, List<String> aErrors)
+        {
+            StreamWriter file = null;
+            try
+            {
+                String errorFile = aSourceDirectory + @"/errors.txt";
+                if (File.Exists(errorFile))
+                    File.Delete(errorFile);
+
+                file = new StreamWriter(errorFile);
+                foreach(String error in aErrors)
+                  file.WriteLine("- {0}", error);
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (file != null)
+                    file.Close();
+            }
+        }
+    }
+}
---
+++ trunk/CS4BB/CS4BB/SourceCode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CS4BB
+{
+    class SourceCode
+    {
+        private FileInfo sourceFile;
+        private List<String> sourceCode = new List<String>();
+
+        public SourceCode(FileInfo aSourceFile)
+        {
+            this.sourceFile = aSourceFile;
+            ReadSourceCode();
+        }
+
+        private void ReadSourceCode()
+        {
+            StreamReader file = null;
+            try
+            {
+                file = new StreamReader(sourceFile.FullName);
+                String line = null;
+                while ((line = file.ReadLine()) != null)
+                    sourceCode.Add(line);
+            }
+            finally
+            {
+                if (file != null)
+                    file.Close();
+            }
+        }
+
+        /// <summary>
+        /// Indicate if the file does contain source code
+        /// </summary>
+        /// <returns></returns>
+        public bool DoesHaveCode()
+        {
+            return this.sourceCode != null && this.sourceCode.Count > 0;
+        }
+
+        /// <summary>
+        /// Get the source file name
+        /// </summary>
+        /// <returns></returns>
+        public String GetFileName()
+        {
+            return this.sourceFile.Name;
+        }
+    }
+}
