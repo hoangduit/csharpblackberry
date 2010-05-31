bfb94831dac91d3571c369af24e46a4f0b40a465
lenniedg <lenniedg@e78c83d8-92f2-14f2-9b48-f43d6f5d67e1>
2010-05-31 22:07:40 +0000

    Fix bug where for an interface the open brace can also b on the same line or on the next line code line

---
+++ CS4BB/InterfacesDemo/ICalculate.java
@@ -0,0 +1,10 @@
+//using System.Collections.Generic;  // Not supported yet
+//using System.Linq;  // Not supported yet
+//using System.Text;  // Not supported yet
+ 
+package InterfacesDemo;
+
+interface IAddCalculate
+{
+int Add(int a, int b);
+}
---
+++ CS4BB/InterfacesDemo/ISubtractCalculate.java
@@ -0,0 +1,10 @@
+//using System.Collections.Generic;  // Not supported yet
+//using System.Linq;  // Not supported yet
+//using System.Text;  // Not supported yet
+ 
+package InterfacesDemo;
+
+interface ISubtractCalculate
+{
+int Subtract(int a, int b);
+}
